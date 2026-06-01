9b578e23a14d2667a42c1227593895331732a8b8
Diyorbek Alikuziev <159881022+dalikuziev@users.noreply.github.com>
2026-06-02 01:04:37 +0500

    Fix echo message formatting in echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -4,5 +4,5 @@ router = Router()
 
 @router.message()
 async def echo_handler(msg: types.Message):
-    await msg.answer(f"Bizda {msg.text} buyrug'i yo'q 😁\nIltimos biz taklif qilgan bo'limlardan tanlang! 👇")
+    await msg.answer(f"Bizda {msg.text} buyrug'i yo'q\nIltimos biz taklif qilgan bo'limlardan tanlang! 👇")
     await msg.answer("Quyidagi bo'limlardan birini tanlang:", reply_markup=yonalish)
